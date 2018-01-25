b05b5560e1008037e3ffc244dbe2a9f251c97b4b
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-01-25 11:29:50 -0500

    update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='mo-files',
-    version="1.7.18011",
+    version="1.7.18025",
     description='More Files! Steamlined for UTF8 and JSON.',
     long_description=long_desc,
     author='Kyle Lahnakoski',
