06f46d44b17c1cac2ececb0e882b818cb77e128d
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-01-28 22:06:10 -0500

    update version number

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 from __future__ import unicode_literals
 from setuptools import setup
 setup(
-    description=str(u'More Files! Steamlined for UTF8 and JSON.'),
-    license=str(u'MPL 2.0'),
-    author=str(u'Kyle Lahnakoski'),
-    author_email=str(u'kyle@lahnakoski.com'),
-    long_description_content_type=str(u'text/markdown'),
+    description=u'More Files! Steamlined for UTF8 and JSON.',
+    license=u'MPL 2.0',
+    author=u'Kyle Lahnakoski',
+    author_email=u'kyle@lahnakoski.com',
+    long_description_content_type=u'text/markdown',
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 2.7","Programming Language :: Python :: 3.7","Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"],
-    install_requires=["mo-dots>=3.22.19344","mo-future>=3.17.19324","mo-logs>=3.21.19344"],
-    version=str(u'3.26.19346'),
-    url=str(u'https://github.com/klahnakoski/mo-files'),
-    python_requires=str(u'==2.7, >=3.6'),
+    install_requires=["mo-dots>=3.33.20028","mo-future>=3.32.20028","mo-logs>=3.38.20029"],
+    version=u'3.38.20029',
+    url=u'https://github.com/klahnakoski/mo-files',
+    python_requires=u'==2.7, >=3.6',
     zip_safe=False,
     packages=["mo_files"],
-    long_description=str(u'More Files!\n==========\n\nThe `File` class makes the default assumption all files have cr-delimited unicode content that is UTF-8 encoded. This is great for JSON files. It also provides better OO over some common file manipulations.\n\n'),
-    name=str(u'mo-files')
+    long_description=u'More Files!\n==========\n\nThe `File` class makes the default assumption all files have cr-delimited unicode content that is UTF-8 encoded. This is great for JSON files. It also provides better OO over some common file manipulations.\n\n',
+    name=u'mo-files'
 )
