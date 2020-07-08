33ed76bcd90ec364478b1773d4330074f299a9d4
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-07-08 07:09:13 -0400

    update version number

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     classifiers=["Programming Language :: Python :: 2.7","Programming Language :: Python :: 3.7","Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"],
     description='More Files! Steamlined for UTF8 and JSON.',
     include_package_data=True,
-    install_requires=["mo-dots>=3.53.20059","mo-future>=3.51.20059","mo-logs>=3.57.20089"],
+    install_requires=["mo-dots>=3.77.20190","mo-future>=3.71.20168","mo-logs>=3.75.20189"],
     license='MPL 2.0',
-    long_description='More Files!\n==========\n\nThe `File` class makes the default assumption all files have cr-delimited unicode content that is UTF-8 encoded. This is great for JSON files. It also provides better OO over some common file manipulations.\n\n',
+    long_description='More Files!\n==========\n\nThe `File` class makes the default assumption all files have cr-delimited unicode content that is UTF-8 encoded. This is great for JSON files. It also provides better OO over some common file manipulations.\n\n\n\n',
     long_description_content_type='text/markdown',
     name='mo-files',
     packages=["mo_files"],
     url='https://github.com/klahnakoski/mo-files',
-    version='3.60.20091',
+    version='3.77.20190',
     zip_safe=False
 )
