f44924211b1e37f85eb7621c41b7df101e5379f9
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-01-04 23:25:55 -0500

    update version number

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     description='More Files! Steamlined for UTF8 and JSON.',
     extras_require={"tests":["mo-testing"]},
     include_package_data=True,
-    install_requires=["mo-dots==9.300.22349","mo-future==7.298.22349","mo-json==6.330.23004","mo-logs==7.330.23004","mo-math==7.325.22362"],
+    install_requires=["mo-dots==9.331.23005","mo-future==7.298.22349","mo-json==6.331.23005","mo-logs==7.331.23005","mo-math==7.331.23005"],
     license='MPL 2.0',
     long_description="# More Files!\n\nThe `File` class makes the default assumption all files have cr-delimited unicode content that is UTF-8 encoded. This is great for JSON files. It also provides better operators over some common file manipulations.\n\n\n\n\n\n\n\n## Recent changes\n\n**Version 6.x**\n\nGet a little closer to Python's pathlib module standards\n\n* `stem` - to refer file name without extension\n* `os_path` - to get the os-specific absolute path for use in other Python modules\n* `rel_path` - the given path \n* `abs_path` - added underscore for consistency \n\n\n\n",
     long_description_content_type='text/markdown',
     name='mo-files',
     packages=["mo_files"],
     url='https://github.com/klahnakoski/mo-files',
-    version='6.330.23004',
+    version='6.331.23005',
     zip_safe=False
 )
